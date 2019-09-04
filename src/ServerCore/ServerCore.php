0c1837960d1057368b55be9e1c221c9ba79e4bcc
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-09-03 17:57:43 -0700

    Update ServerCore.php

--- src/ServerCore/ServerCore.php
+++ src/ServerCore/ServerCore.php
@@ -19,7 +19,7 @@ class ServerCore extends PluginBase{
   }
   
   public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    if(strtolower($cmd->getName()) == "Done"){
+    if(strtolower($cmd->getName()) == "heal"){
       if($sender->hasPermission("Mod")){
         $sender->sendMessage(c::BOLD.c::DARK_AQUA."(!)"c.::DARK_PURPLE." You have been healed");
         $sender->setHealth(20.0);
