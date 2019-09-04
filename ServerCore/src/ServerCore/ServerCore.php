426fb3d1cd9a4afcc4b47f85a68e2c97d68d57ae
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-09-03 20:36:24 -0700

    Update ServerCore.php

--- ServerCore/src/ServerCore/ServerCore.php
+++ ServerCore/src/ServerCore/ServerCore.php
@@ -25,5 +25,5 @@ class ServerCore extends PluginBase{
   public function onDisable(){
     $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." ServerCore has been disabled")
      
-   }
+   
 
