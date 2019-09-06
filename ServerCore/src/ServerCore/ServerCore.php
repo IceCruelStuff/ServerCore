bede2de2fa630e24bf33587edc3e8b6be07f5e08
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-09-06 15:38:06 -0700

    fix error

--- ServerCore/src/ServerCore/ServerCore.php
+++ ServerCore/src/ServerCore/ServerCore.php
@@ -22,6 +22,7 @@ class ServerCore extends PluginBase{
     }
   }
   
-public function onDisable(){
-    $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." ServerCore has been disabled");
+  public function onDisable(){
+      $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." ServerCore has been disabled");
+  }
 }
