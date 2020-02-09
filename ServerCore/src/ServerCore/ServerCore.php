53e5e464aa06a8153a84565e3c1472433c4fe677
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-02-09 11:54:34 -0800

    update functions

--- ServerCore/src/ServerCore/ServerCore.php
+++ ServerCore/src/ServerCore/ServerCore.php
@@ -1,17 +1,21 @@
 <?php
+
 namespace ServerCore;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat as c;
+
 class ServerCore extends PluginBase{
-  public function onEnable() : void{
+  
+  public function onEnable() : void {
     $this->getLogger()->notice(c::BOLD.c::DARK_AQUA."(!)".c::RESET.c::DARK_PURPLE." ServerCore has been enabled");
   }
   
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
     if(strtolower($command->getName()) == "heal"){
       if($sender->hasPermission("Mod")){
         $sender->sendMessage(c::BOLD.c::DARK_AQUA."(!)",c::DARK_PURPLE." You have been healed");
@@ -22,7 +26,7 @@ class ServerCore extends PluginBase{
     }
   }
   
-  public function onDisable(){
+  public function onDisable() {
       $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." ServerCore has been disabled");
   }
 }
