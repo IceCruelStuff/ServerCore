64d452351eb1983a2e6d963d650e71559db65da9
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-12 12:29:49 -0700

    Update ServerCore.php

--- src/ServerCore/ServerCore.php
+++ src/ServerCore/ServerCore.php
@@ -2,11 +2,14 @@
 
 namespace ServerCore;
 
-use pocketmine\plugin\PluginBase;
+use ServerCore\AntiHack\AntiHackLoader;
+use ServerCore\Elytra\Main;
+use ServerCore\Fireworks\Loader;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
 use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as c;
 
 class ServerCore extends PluginBase{
@@ -16,18 +19,19 @@ class ServerCore extends PluginBase{
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if(strtolower($command->getName()) == "heal"){
-            if($sender->hasPermission("Mod")){
-                $sender->sendMessage(c::BOLD.c::DARK_AQUA."(!)",c::DARK_PURPLE." You have been healed");
-                $sender->setHealth(20.0);
-            } elseif(!$sender->hasPermission("Mod")){
-                $sender->sendMessage(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." You do not have permission to run this command");
-            }
+        switch($command->getName()) {
+            case "heal":
+                if($sender->hasPermission("healcommand")){
+                    $sender->sendMessage(c::BOLD.c::DARK_AQUA."(!)",c::DARK_PURPLE." You have been healed");
+                    $sender->setHealth(20.0);
+                } elseif(!$sender->hasPermission("healcommand")){
+                    $sender->sendMessage(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." You do not have permission to run this command");
+                }
         }
-        return true;
+        return false;
     }
 
-    public function onDisable() {
+    public function onDisable() : void {
         $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." ServerCore has been disabled");
     }
 }
