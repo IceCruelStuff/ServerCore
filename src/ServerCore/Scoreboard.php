d3c81ad30b3679dfa949517236edeef6c9f26e14
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-21 19:26:38 -0700

    Update Scoreboard.php

--- src/ServerCore/Scoreboard.php
+++ src/ServerCore/Scoreboard.php
@@ -61,11 +61,11 @@ class Scoreboard implements Listener {
 
     public function setLine(Player $player, int $score, string $message) : void {
         if (!isset($this->scoreboards[$player->getName()])) {
-            $plugin->getLogger()->error("Cannot set a score to a player with no scoreboard");
+            $this->plugin->getLogger()->error("Cannot set a score to a player with no scoreboard");
             return;
         }
         if ($score > 15 || $score < 1) {
-            $plugin->getLogger()->error("Score must be between the value of 1-15. $score out of range");
+            $this->plugin->getLogger()->error("Score must be between the value of 1-15. $score out of range");
             return;
         }
         $objectiveName = $this->getObjectiveName($player);
