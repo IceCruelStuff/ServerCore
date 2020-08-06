4e766cfb38e99fcb89dc484aedfc502109788c83
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-08-05 20:50:26 -0700

    Update ServerCore.php

--- src/ServerCore/ServerCore.php
+++ src/ServerCore/ServerCore.php
@@ -145,6 +145,7 @@ class ServerCore extends PluginBase implements Listener {
             $this->getLogger()->error("Cannot set a score to a player with no scoreboard");
             return;
         }
+
         if ($score > 15 || $score < 1) {
             $this->getLogger()->error("Score must be between the value of 1-15. $score out of range");
             return;
