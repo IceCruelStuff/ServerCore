4b8b1431d81b30440916d4964b3875ab3d0d893d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-28 22:12:37 -0800

    Create PingCommand.php

---
+++ src/ServerCore/Command/PingCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ServerCore\Command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use ServerCore\ServerCore as Main;
+
+class PingCommand extends Command implements PluginIdentifiableCommand {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+        parent::__construct(
+            "ping",
+            "Returns player ping",
+            "/ping",
+            ["ms"]
+        );
+        $this->setPermission("command.ping");
+    }
+
+    public function getPlugin() : Plugin {
+        return $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args) {
+        if (!$this->testPermission($sender)) {
+            return;
+        }
+
+        if ($sender instanceof Player) {
+            $sender->sendMessage("Ping: " . $sender->getPing() . "ms");
+        } else {
+            $sender->sendMessage(TextFormat::RED . "Please use this command in-game");
+        }
+    }
+
+}
