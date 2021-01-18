09351098a6bf0a03ebd01f8b1cf798aaf0f4a61f
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-01-17 18:53:20 -0800

    Update SmiteCommand.php

--- src/ServerCore/command/SmiteCommand.php
+++ src/ServerCore/command/SmiteCommand.php
@@ -62,23 +62,27 @@ class SmiteCommand extends Command implements PluginIdentifiableCommand {
                 $sender->sendMessage(TextFormat::RED . "Player not found");
             }
         } else {
-            $pk = new AddActorPacket();
-            $pk->type = "minecraft:lightning_bolt";
-            $pk->entityRuntimeId = Entity::$entityCount++;
-            $pk->metadata = [];
-            $pk->motion = null;
-            $pk->yaw = $sender->getYaw();
-            $pk->pitch = $sender->getPitch();
-            $pk->position = new Vector3($sender->getX(), $sender->getY(), $sender->getZ());
-            Server::getInstance()->broadcastPacket($sender->getLevel()->getPlayers(), $pk);
-            $sound = new PlaySoundPacket();
-            $sound->soundName = "ambient.weather.thunder";
-            $sound->x = $sender->getX();
-            $sound->y = $sender->getY();
-            $sound->z = $sender->getZ();
-            $sound->volume = 1;
-            $sound->pitch = 1;
-            Server::getInstance()->broadcastPacket($sender->getLevel()->getPlayers(), $sound);
+            if ($sender instanceof Player) {
+                $pk = new AddActorPacket();
+                $pk->type = "minecraft:lightning_bolt";
+                $pk->entityRuntimeId = Entity::$entityCount++;
+                $pk->metadata = [];
+                $pk->motion = null;
+                $pk->yaw = $sender->getYaw();
+                $pk->pitch = $sender->getPitch();
+                $pk->position = new Vector3($sender->getX(), $sender->getY(), $sender->getZ());
+                Server::getInstance()->broadcastPacket($sender->getLevel()->getPlayers(), $pk);
+                $sound = new PlaySoundPacket();
+                $sound->soundName = "ambient.weather.thunder";
+                $sound->x = $sender->getX();
+                $sound->y = $sender->getY();
+                $sound->z = $sender->getZ();
+                $sound->volume = 1;
+                $sound->pitch = 1;
+                Server::getInstance()->broadcastPacket($sender->getLevel()->getPlayers(), $sound);
+            } else {
+                $sender->sendMessage(TextFormat::RED . "Please enter a player name");
+            }
         }
     }
 
