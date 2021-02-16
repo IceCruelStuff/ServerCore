63819617c39c07144f69ca00f8f3c132a78ad085
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-02-15 20:59:14 -0800

    Create MuteCommand.php

---
+++ src/ServerCore/command/MuteCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ServerCore\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use ServerCore\ServerCore as Main;
+
+class MuteCommand extends Command implements PluginIdentifiableCommand {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+        parent::__construct(
+            "mute",
+            "Mutes the player",
+            "/mute <player> <reason>"
+        );
+        $this->setPermission("command.mute");
+    }
+
+    public function getPlugin() : Plugin {
+        return $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args) {
+        if (!$this->testPermission($sender)) {
+            return;
+        }
+
+        // TODO
+    }
+
+}
