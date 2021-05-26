c18b8b4ede6cc80803d5fbeb3cc672d246429c6f
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-05-25 17:59:03 -0700

    Create Scoreboard.php

---
+++ src/ServerCore/scoreboard/Scoreboard.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace ServerCore\scoreboard;
+
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\Player;
+
+class Scoreboard {
+
+    private $scoreboards = [];
+
+    public function new(Player $player, string $objectiveName, string $displayName) : void {
+        if (isset($this->scoreboards[$player->getName()])) {
+            $this->remove($player);
+        }
+
+        $pk = new SetDisplayObjectivePacket();
+        $pk->displaySlot = "sidebar";
+        $pk->objectiveName = $objectiveName;
+        $pk->displayName = $displayName;
+        $pk->criteriaName = "dummy";
+        $pk->sortOrder = 0;
+        $player->sendDataPacket($pk);
+        $this->scoreboards[$player->getName()] = $objectiveName;
+    }
+
+    public function remove(Player $player) : void {
+        $objectiveName = $this->getObjectiveName($player);
+        $pk = new RemoveObjectivePacket();
+        $pk->objectiveName = $objectiveName;
+        $player->sendDataPacket($pk);
+        unset($this->scoreboards[$player->getName()]);
+    }
+
+    public function setLine(Player $player, int $score, string $message) : void {
+        if (!isset($this->scoreboards[$player->getName()])) {
+            $this->getLogger()->error("Cannot set a score to a player with no scoreboard");
+            return;
+        }
+
+        if ($score > 15 || $score < 1) {
+            $this->getLogger()->error("Score must be between the value of 1-15. " . $score . " out of range");
+            return;
+        }
+
+        $objectiveName = $this->getObjectiveName($player);
+        $entry = new ScorePacketEntry();
+        $entry->objectiveName = $objectiveName;
+        $entry->type = $entry::TYPE_FAKE_PLAYER;
+        $entry->customName = $message;
+        $entry->score = $score;
+        $entry->scoreboardId = $score;
+        $pk = new SetScorePacket();
+        $pk->type = $pk::TYPE_CHANGE;
+        $pk->entries[] = $entry;
+        $player->sendDataPacket($pk);
+    }
+
+    public function getObjectiveName(Player $player) : ?string {
+        return isset($this->scoreboards[$player->getName()]) ? $this->scoreboards[$player->getName()] : null;
+    }
+
+}
